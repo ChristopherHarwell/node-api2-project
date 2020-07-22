c9cba98e817861c395ac6777a785642806bfaca5
Chris <chrisharwell72@gmail.com>
2020-07-22 08:58:22 -0400

    Added get post and delete endpoints for posts.router.js
    Need to add functionality to those requests and build out posts.model.js later

--- posts/posts.router.js
+++ posts/posts.router.js
@@ -1,3 +1,15 @@
 const express = require("express");
 
 // add require for post.model.js later
+
+const router = express.Router();
+
+router.get("/", (req, res) => {});
+
+router.get("/:id", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+router.delete(":/id", (req, res) => {});
+
+router.put(":/id", (req, res) => {});
